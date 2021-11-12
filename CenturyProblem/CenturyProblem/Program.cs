74743de152f3cfd7b1f13aa66a5fb1b0409d6cf3
BeeWeak <86132156+BeeWeak@users.noreply.github.com>
2021-11-12 18:04:50 +0000

    Add files via upload

---
+++ CenturyProblem/CenturyProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CenturyProblem
+{
+    class Program
+    {
+        static void Century(int year)
+        {
+            int value = ((year - 1) / 100) + 1;
+            Console.WriteLine(value);     
+        }
+        static void Main(string[] args)
+        {
+            Century(1999);
+            Century(2000);
+            Century(2020);
+        }
+    }
+}
