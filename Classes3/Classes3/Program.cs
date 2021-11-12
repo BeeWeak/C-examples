c3e19a41eae0c61cd6fcc7f467b400aaff713fef
BeeWeak <86132156+BeeWeak@users.noreply.github.com>
2021-11-12 18:06:30 +0000

    Add files via upload

---
+++ Classes3/Classes3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes3
+{
+    class Shape
+    {
+        public string Type = "square";
+        public int Sides = 4;
+        public int Sidelength = 1;
+        public double Area = 1;
+    }
+
+
+
+}
---
+++ classProperties/classProperties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace classProperties
+{
+    public class Shape
+    {
+        private int Sides;
+        public int GetSides()
+        {
+            return Sides;
+        }
+
+        public void SetSides(int value)
+        {
+            if (value > 2) Sides = value;
+        }
+    }
+}
